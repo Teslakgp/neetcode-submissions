c13f25127733b258f0ed93dc61aa20517f9af9b6
Shreenivas Chityala <62350960+Teslakgp@users.noreply.github.com>
2026-06-29 16:45:49 +0530

    Bulk sync: 4 submissions

---
+++ Data Structures & Algorithms/climbing-stairs/submission-6.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int climbStairs(int n) {
+        if(n <= 2) return n;
+        int one = 1;
+        int two = 2;
+        int ans = 0;
+        for(int i = 3; i <= n; i++){
+            ans = one + two;
+            one = two;
+            two = ans;
+        }
+        return ans;
+    }
+}
